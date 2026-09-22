7602f71f19a88bf6ad51101a694307570b705e88
Ilenard3 <isaaclenard3@gmail.com>
2026-09-22 14:12:30 -0400

    recommit script d7

--- day7/script.js
+++ day7/script.js
@@ -3,10 +3,9 @@ const damageAmount = 30;
 
 function takeDamage() {
     playerHealth = playerHealth - damageAmount;
-    const healthText = document.getElementById("health-display")''
+    
+    const healthText = document.getElementById("health-display");
     const statusText = document.getElementById("status-message");
-
-    healthText.innerText  = playerHealth;
 
     if (playerHealth > 0) {
         healthText.innerText  = playerHealth;
