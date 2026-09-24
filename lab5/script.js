e347cf8df59f220dc333b8ef388e4285d79cb5de
Ilenard3 <isaaclenard3@gmail.com>
2026-09-24 13:28:47 -0400

    Lab 5 Inital commit

---
+++ lab5/script.js
@@ -0,0 +1,27 @@
+let accountBalance = 0;
+const DepositAmount = 30;
+
+function DepositAmount() {
+    accountBalance = accountBalance + DepositAmount;
+    
+    const healthText = document.getElementById("health-display");
+    const statusText = document.getElementById("status-message");
+
+    if (playerHealth > 0) 
+    {
+        healthText.innerText  = playerHealth;
+        statusText.innerText = "You've been hit!";
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "Game Over!";
+        statusText.style.color = "#f9331d";
+        statusText.style.fontWeight = "bold";
+
+        document.body.style.backgroundColor = "#5a1a1a";
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "Dead";
+    }
+}
+
