413c687fcab7f02f17db91a46da21181e192454c
Ilenard3 <isaaclenard3@gmail.com>
2026-09-24 13:42:31 -0400

    Withdraw commit

--- lab5/script.js
+++ lab5/script.js
@@ -1,27 +1,16 @@
 let accountBalance = 0;
 const depositAmount = 10;
+const withdrawAmount = 10;
 
 function depositAmount() {
     accountBalance = accountBalance + depositAmount;
     
     const accountText = document.getElementById("Account-display");
     const despositText = document.getElementById("desposit-message");
-
-    if (accountBalance > 0) 
-    {
-        healthText.innerText  = playerHealth;
-        statusText.innerText = "You've been hit!";
-    }
-    else
-    {
-        healthText.innerText = 0;
-        statusText.innerText = "Game Over!";
-        statusText.style.color = "#f9331d";
-        statusText.style.fontWeight = "bold";
-
-        document.body.style.backgroundColor = "#5a1a1a";
-        document.querySelector("button").disabled = true;
-        document.querySelector("button").innerText = "Dead";
-    }
 }
-
+function withdrawAmount() {
+    accountBalance = accountBalance - withdrawAmount;
+    
+    const accountText = document.getElementById("Account-display");
+    const despositText = document.getElementById("withdraw-message");
+}
