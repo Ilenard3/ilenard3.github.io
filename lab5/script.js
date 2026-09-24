7973e30b3f62db78aef5c1db2b679fcc09901540
Ilenard3 <isaaclenard3@gmail.com>
2026-09-24 13:47:19 -0400

    if statement

--- lab5/script.js
+++ lab5/script.js
@@ -14,3 +14,19 @@ function withdrawAmount() {
     const accountText = document.getElementById("Account-display");
     const despositText = document.getElementById("withdraw-message");
 }
+ if (accountBalance > 100) 
+    {
+        healthText.innerText  = playerHealth;
+        statusText.innerText = "You are rich";
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "No Money";
+        statusText.style.color = "#f9331d";
+        statusText.style.fontWeight = "bold";
+
+        document.body.style.backgroundColor = "#5a1a1a";
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "Dead";
+    }
